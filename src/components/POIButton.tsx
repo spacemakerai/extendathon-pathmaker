d2d00e5f388d39cf65020ccc9f509b82ea9f393c
Kjetil Bugge Kristoffersen <kjetil.kristoffersen@autodesk.com>
2023-11-30 16:49:41 +0100

    feat: add unlimited points

--- src/components/POIButton.tsx
+++ src/components/POIButton.tsx
@@ -2,16 +2,21 @@ import { useCallback } from "preact/compat";
 import { Forma } from "forma-embedded-view-sdk/auto";
 import state from "../pathmaker/state.ts";
 import { coordinateToCanvasSpace } from "../pathmaker/helpers.ts";
+import { useState } from "preact/hooks";
 
 export default function POIButton() {
+  const [settingPOI, setSettingPOI] = useState(false);
   const onClick = useCallback(async () => {
+    setSettingPOI(true);
     const pos = await Forma.designTool.getPoint();
     if (pos) {
       state.pointsOfInterest.value = [
         ...state.pointsOfInterest.value,
         coordinateToCanvasSpace(pos),
       ];
+      await onClick();
     }
+    setSettingPOI(false);
   }, []);
 
   return (
@@ -22,7 +27,7 @@ export default function POIButton() {
         <p>{state.pointsOfInterest.value.length} POIs added</p>
       ) : null}
       <weave-button variant="outlined" onClick={onClick}>
-        Add POI (store, bus stop, etc.)
+        {settingPOI ? "Press escape to complete" : "Add POI (store, bus stop, etc.)"}
       </weave-button>
     </div>
   );
--- src/components/SourcePointButton.tsx
+++ src/components/SourcePointButton.tsx
@@ -2,13 +2,19 @@ import { useCallback } from "preact/compat";
 import { Forma } from "forma-embedded-view-sdk/auto";
 import state from "../pathmaker/state.ts";
 import { coordinateToCanvasSpace } from "../pathmaker/helpers.ts";
+import { useState } from "preact/hooks";
 
 export default function SourcePointButton() {
+  const [settingSourcePoints, setSettingSourcePoints] = useState(false);
   const onClick = useCallback(async () => {
+    setSettingSourcePoints(true);
     const pos = await Forma.designTool.getPoint();
     if (pos) {
       state.sourcePoints.value = [...state.sourcePoints.value, coordinateToCanvasSpace(pos)];
+      await onClick();
     }
+    setSettingSourcePoints(false);
+    return;
   }, []);
 
   return (
@@ -19,7 +25,7 @@ export default function SourcePointButton() {
         <p>{state.sourcePoints.value.length} source points added</p>
       ) : null}
       <weave-button variant="outlined" onClick={onClick}>
-        Add source point
+        {settingSourcePoints ? "Press escape to complete" : "Add source points"}
       </weave-button>
     </div>
   );
